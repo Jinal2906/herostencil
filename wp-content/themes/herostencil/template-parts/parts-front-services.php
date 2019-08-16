6f0d91b95c01d4ee7bbe0af5341ad127160fa785
gaurav-zwt <gaurav@zealousweb.com>
2019-08-16 21:15:27 +0530

    added positioning classes added mixin for rgba color, completed services section with responsive with classes oriented methodology

--- wp-content/themes/herostencil/template-parts/parts-front-services.php
+++ wp-content/themes/herostencil/template-parts/parts-front-services.php
@@ -11,16 +11,12 @@ if(have_rows('services')){
     while( have_rows('services') ) :
     the_row();
 
-    echo '<section class="services-section"';
-        if( !empty( $ServicesSecBG ) ) {
-            echo 'style="background-image: url(' . $ServicesSecBG . ')"';
-        }
-        echo '>' .
+    echo '<section class="services-section position-relative py-50 text-center"' . ( !empty( $ServicesSecBG ) ?  'style="background-image: url(' . $ServicesSecBG . ')"' : '' ) . '>' .
         '<div class="wrapper">' .
         '<h2>' . $ServicesSecTitle . '</h2>' .
         '<p>' . $ServicesSecDesc . '</p>';
             if( have_rows('services_list') ){
-            echo '<ul>';
+            echo '<ul class=" d-flex list-none row-15 p-0 ">';
             while ( have_rows('services_list') ) :
                 the_row();
                 $serviceIcon = get_sub_field('services_icon');
@@ -28,24 +24,24 @@ if(have_rows('services')){
                 $serviceUrl = get_sub_field('service_url');
                 $serviceDesc = get_sub_field('service_desc');
 
-                $img_html = get_image( $serviceIcon, 'large', true );
+                $img_html = wp_get_attachment_image( $serviceIcon, 'large' );
 
-                echo '<li>' .
-                        '<div class="service-image innbaner">' .
+                echo '<li class=" position-relative d-block cell-4 p-15 cell-992-6 cell-480-12 ">' .
+                        '<div class="service-image innbaner image-src pt-75 cell-12 mb-10 position-relative block-animate">' .
                             (
-                                get_sub_field('services_icon')
+                                $img_html
                                 ? $img_html
                                 : ''
                             ) .
-                            '<div class="service-content">' .
+                            '<div class="service-content width-full height-full p-15 d-flex justify-content-center align-content-center position-absolute position-top-0-px position-left-0-px transition opacity-0 ">' .
                                 (
                                     get_sub_field('service_title')
-                                    ? '<h3>' . get_sub_field('service_title') . '</h3>'
+                                    ? '<h3 class="mb-10">' . get_sub_field('service_title') . '</h3>'
                                     : ''
                                 ) .
                                 (
                                     get_sub_field('service_desc')
-                                    ? '<p>' . get_sub_field('service_desc') . '</p>'
+                                    ? '<p class="mb-10">' . get_sub_field('service_desc') . '</p>'
                                     : ''
                                 ) .
                                 (
